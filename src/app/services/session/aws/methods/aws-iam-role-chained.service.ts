808659ad0b415e7232a912e5e4b3875807f327fe
Eric Villa <eric.villa91@gmail.com>
2021-10-26 16:13:53 +0200

    fix: fixed import statements in aws iam role chained service

--- src/app/services/session/aws/methods/aws-iam-role-chained.service.ts
+++ src/app/services/session/aws/methods/aws-iam-role-chained.service.ts
@@ -15,7 +15,8 @@ import {KeychainService} from '../../../keychain.service';
 import {AwsIamUserService} from './aws-iam-user.service';
 import {AwsSsoRoleService} from './aws-sso-role.service';
 import {ElectronService} from '../../../electron.service';
-import {AwsSsoOidcService} from "../../../aws-sso-oidc.service";
+import {AwsSsoOidcService} from '../../../aws-sso-oidc.service';
+import { AssumeRoleResponse } from 'aws-sdk/clients/sts';
 
 export interface AwsIamRoleChainedSessionRequest {
   accountName: string;
