b2872f89b535ebed467321cb3cf28e0301bc286f
Marco Vanetti <marco.vanetti@gmail.com>
2022-01-26 12:19:04 +0100

    feat: working on desktop app gushio scripts

---
+++ desktop-app/gushio/makedir-func.js
@@ -0,0 +1,5 @@
+module.exports = async function makeDirFunction(path, relativeDirPath) {
+    const dirPath = path.join(__dirname, relativeDirPath)
+    await fs.mkdirp(dirPath)
+
+}
--- desktop-app/gushio/target-build.js
+++ desktop-app/gushio/target-build.js
@@ -1,25 +1,36 @@
+const makeDirFunction = require('./makedir-func')
 const path = require('path')
+const copyFunction = require('./copy-func')
 module.exports = {
-  cli: {
-    name: 'build',
-    description: 'Build the leapp core library',
-    version: '0.1',
-  },
-  run: async () => {
-    const path = require('path')
-    const shellJs = require('shelljs')
-    const deleteFunction = require('./delete-func')
-    const compileFunction = require('./compile-func')
+    cli: {
+        name: 'build',
+        description: 'Build leapp desktop app',
+        version: '0.1',
+        arguments: [
+            {name: '<target>', choices: ['aot', 'configuration staging', 'configuration production']},
+        ],
+    },
+    run: async (args) => {
+        const path = require('path')
+        const shellJs = require('shelljs')
+        const deleteFunction = require('./delete-func')
+        const copyFunction = require('./copy-func')
+        const makeDirFunction = require('./makedir-func')
+        const compileFunction = require('./compile-func')
+        try {
+            console.log('Building leapp... ')
 
-    try {
-      console.log('Building leapp-core library... ')
+            await deleteFunction(path, '../electron/dist')
+            await deleteFunction(path, '../dist')
+            await makeDirFunction(path, '../dist/leapp-client')
+            await copyFunction(path, '../src/assets/icons', '../dist/leapp-client')
+            await compileFunction(shellJs, args[0])
+            await makeDirFunction(path, '../electron/dist/electron/assets/images')
+            await copyFunction(path, '../electron/assets/images', '../electron/dist/electron/assets/images')
 
-      await deleteFunction(path, '../dist')
-      await compileFunction(shellJs)
-
-      console.log('Build completed successfully')
-    } catch (e) {
-      console.error(e.message.red)
-    }
-  },
+            console.log('Build completed successfully')
+        } catch (e) {
+            console.error(e.message.red)
+        }
+    },
 }
