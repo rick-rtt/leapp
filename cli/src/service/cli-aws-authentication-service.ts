98857f06b961d544c46922f0146a0337c89ecb38
Fabio Gozzo <fabio.gozzo@gmail.com>
2022-02-04 17:10:33 +0100

    spike: puppeteer

--- cli/src/service/cli-aws-authentication-service.ts
+++ cli/src/service/cli-aws-authentication-service.ts
@@ -1,8 +1,28 @@
 import { IAwsAuthenticationService } from '@noovolari/leapp-core/interfaces/i-aws-authentication.service'
+import puppeteer from 'puppeteer'
 
 export class CliAwsAuthenticationService implements IAwsAuthenticationService {
   async needAuthentication(idpUrl: string): Promise<boolean> {
-    return idpUrl === ''
+    const browser = await puppeteer.launch({headless: false})
+    const page = await browser.newPage()
+    await page.setRequestInterception(true)
+    page.on('request', async (request) => {
+
+      if ((request.url.toString().indexOf('https://accounts.google.com/ServiceLogin') !== -1) &&
+        (request.url.toString().indexOf('.onelogin.com/login') !== -1) &&
+        (request.url.toString().indexOf('.okta.com/discovery/iframe.html') !== -1)  &&
+        (request.url.toString().indexOf('https://login.microsoftonline.com') !== -1) &&
+        (request.url.toString().indexOf('/oauth2/authorize') !== -1)){
+      }
+      else
+      {
+        console.log(request)
+        await browser.close()
+      }
+
+    })
+    await page.goto(idpUrl)
+    return true
   }
 
 
