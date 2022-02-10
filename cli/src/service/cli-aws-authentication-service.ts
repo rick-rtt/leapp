d98a516e0a812d45ce4c81fa1f69ac05d98d94c4
faxmaister <fabio.gozzo@gmail.com>
2022-02-10 17:09:25 +0100

    fix: CliAwsAuthenticationService test

--- cli/src/service/cli-aws-authentication-service.ts
+++ cli/src/service/cli-aws-authentication-service.ts
@@ -1,70 +1,85 @@
 import { IAwsAuthenticationService } from '@noovolari/leapp-core/interfaces/i-aws-authentication.service'
 import puppeteer from 'puppeteer'
+import { LeappModalClosedError } from '@noovolari/leapp-core/errors/leapp-modal-closed-error'
 
 export class CliAwsAuthenticationService implements IAwsAuthenticationService {
   private browser: puppeteer.Browser
 
-  async needAuthentication(idpUrl: string): Promise<boolean> {
+  public async needAuthentication(idpUrl: string): Promise<boolean> {
     return new Promise(async (resolve, reject) => {
+      const page = await this.getNavigationPage(true)
 
-      this.browser = await puppeteer.launch({headless: true, devtools: false})
-      const page = await this.browser.newPage()
-      await page.setDefaultNavigationTimeout(0)
-      await page.setRequestInterception(true)
-      page.on('request', request => {
+      page.on('request', async request => {
         const requestUrl = request.url().toString()
         if (request.isInterceptResolutionHandled()) {
           reject('request unexpectedly already handled')
+          return
         }
 
         if (this.isRequestToIntercept(requestUrl)) {
-          request.abort()
           resolve(requestUrl.indexOf('https://signin.aws.amazon.com/saml') === -1)
-        } else {
-          request.continue()
+          return
         }
+
+        await request.continue()
       })
 
-      try {await page.goto(idpUrl)}catch (e) {
-      }
+      try { await page.goto(idpUrl) } catch (e) {}
     })
   }
 
   public async awsSignIn(idpUrl: string, needToAuthenticate: boolean): Promise<any> {
     return new Promise(async (resolve, reject) => {
+      const page = await this.getNavigationPage(!needToAuthenticate)
 
-      this.browser = await puppeteer.launch({headless: !needToAuthenticate, devtools: false})
-      const page = await this.browser.newPage()
-      await page.setDefaultNavigationTimeout(0)
-      await page.setRequestInterception(true)
-      page.on('request', request => {
+      page.on('request', async request => {
         const requestUrl = request.url().toString()
         if (request.isInterceptResolutionHandled()) {
           reject('request unexpectedly already handled')
+          return
         }
 
         if (requestUrl.indexOf('https://signin.aws.amazon.com/saml') !== -1) {
-          request.abort()
           resolve({uploadData: [{bytes: {toString: () => request.postData()}}]})
-        } else {
-          request.continue()
+          return
         }
+
+        await request.continue()
       })
 
-      try {await page.goto(idpUrl)}catch (e) {
-      }
+      page.on('close', ()=>{
+        reject(new LeappModalClosedError(this, 'request window closed by user'))
+      })
+
+      try { await page.goto(idpUrl) } catch (e) {}
     })
   }
 
   public async closeAuthenticationWindow(): Promise<void> {
     if (this.browser) {
-      this.browser.removeAllListeners()
-      this.browser.disconnect()
+      for (const page of await this.browser.pages()){
+        page.removeAllListeners()
+        await page.close()
+      }
+
       await this.browser.close()
     }
   }
 
-  private isRequestToIntercept(requestUrl: string): boolean {
+  public async getNavigationPage(headlessMode: boolean): Promise<puppeteer.Page> {
+    this.browser = await puppeteer.launch({headless: headlessMode, devtools: false})
+    const pages = await this.browser.pages()
+    const page = pages.length > 0
+      ? pages[0]
+      : await this.browser.newPage()
+
+    await page.setDefaultNavigationTimeout(180000)
+    await page.setRequestInterception(true)
+
+    return page
+  }
+
+  public isRequestToIntercept(requestUrl: string): boolean {
     if (requestUrl.indexOf('https://login.microsoftonline.com') !== -1 &&
       requestUrl.indexOf('/oauth2/authorize') !== -1) {
       return true
