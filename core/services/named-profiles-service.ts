ea7f9c19f99be3ba3d5f0e0ad51c5ab760543564
Alessandro Alfano <alessandro.alfano@forge.srl>
2022-03-04 12:36:39 +0100

    refactor: core and cli code is now compliant with the new linter

--- core/services/named-profiles-service.ts
+++ core/services/named-profiles-service.ts
@@ -1,86 +1,82 @@
-import * as uuid from 'uuid'
-import {constants} from '../models/constants'
-import {AwsNamedProfile} from '../models/aws-named-profile'
-import {Repository} from './repository'
-import {Session} from '../models/session'
-import {SessionFactory} from './session-factory'
-import {SessionStatus} from '../models/session-status'
-import {WorkspaceService} from './workspace-service'
+import * as uuid from "uuid";
+import { constants } from "../models/constants";
+import { AwsNamedProfile } from "../models/aws-named-profile";
+import { Repository } from "./repository";
+import { Session } from "../models/session";
+import { SessionFactory } from "./session-factory";
+import { SessionStatus } from "../models/session-status";
+import { WorkspaceService } from "./workspace-service";
 
 export class NamedProfilesService {
+  constructor(private sessionFactory: SessionFactory, private repository: Repository, private workspaceService: WorkspaceService) {}
 
-    constructor(private sessionFactory: SessionFactory, private repository: Repository,
-                private workspaceService: WorkspaceService) {
+  getNamedProfiles(excludingDefault: boolean = false): AwsNamedProfile[] {
+    const excludedProfileId = excludingDefault ? this.repository.getDefaultProfileId() : null;
+    return this.repository.getProfiles().filter((profile) => profile.id !== excludedProfileId);
+  }
+
+  getNamedProfilesMap(): Map<string, AwsNamedProfile> {
+    return new Map(this.getNamedProfiles().map((profile) => [profile.id, profile] as [string, AwsNamedProfile]));
+  }
+
+  getSessionsWithNamedProfile(id: string): Session[] {
+    return this.repository.getSessions().filter((session) => (session as any).profileId === id);
+  }
+
+  createNamedProfile(name: string): void {
+    this.repository.addProfile(new AwsNamedProfile(this.getNewId(), name.trim()));
+  }
+
+  async editNamedProfile(id: string, newName: string): Promise<void> {
+    const activeSessions = this.getSessionsWithNamedProfile(id).filter((session) => session.status === SessionStatus.active);
+
+    for (const session of activeSessions) {
+      await this.sessionFactory.getSessionService(session.type).stop(session.sessionId);
     }
-
-    public getNamedProfiles(excludingDefault: boolean = false): AwsNamedProfile[] {
-        const excludedProfileId = excludingDefault ? this.repository.getDefaultProfileId() : null
-        return this.repository.getProfiles().filter(profile => profile.id !== excludedProfileId)
+    this.repository.updateProfile(id, newName.trim());
+    for (const session of activeSessions) {
+      await this.sessionFactory.getSessionService(session.type).start(session.sessionId);
     }
+  }
 
-    getNamedProfilesMap(): Map<string, AwsNamedProfile> {
-        return new Map(this.getNamedProfiles().map(profile => [profile.id, profile] as [string, AwsNamedProfile]))
+  async deleteNamedProfile(id: string): Promise<void> {
+    const sessions = this.getSessionsWithNamedProfile(id);
+    const defaultNamedProfileId = this.repository.getDefaultProfileId();
+
+    for (const session of sessions) {
+      const sessionService = this.sessionFactory.getSessionService(session.type);
+      const wasActive = session.status === SessionStatus.active;
+      if (wasActive) {
+        await sessionService.stop(session.sessionId);
+      }
+
+      (session as any).profileId = defaultNamedProfileId;
+      this.repository.updateSession(session.sessionId, session);
+      this.workspaceService.updateSession(session.sessionId, session);
+
+      if (wasActive) {
+        await sessionService.start(session.sessionId);
+      }
     }
+    this.repository.removeProfile(id);
+  }
 
-    getSessionsWithNamedProfile(id: string): Session[] {
-        return this.repository.getSessions().filter(session => (session as any).profileId === id)
+  getNewId(): string {
+    return uuid.v4();
+  }
+
+  validateNewProfileName(name: string): boolean | string {
+    const trimmedName = name.trim();
+    if (trimmedName.length === 0) {
+      return "Empty profile name";
     }
-
-    createNamedProfile(name: string) {
-        this.repository.addProfile(new AwsNamedProfile(this.getNewId(), name.trim()))
+    if (trimmedName === constants.defaultAwsProfileName) {
+      return '"default" is not a valid profile name';
     }
-
-    async editNamedProfile(id: string, newName: string) {
-        const activeSessions = this.getSessionsWithNamedProfile(id)
-            .filter(session => session.status == SessionStatus.active)
-
-        for (const session of activeSessions) {
-            await this.sessionFactory.getSessionService(session.type).stop(session.sessionId)
-        }
-        this.repository.updateProfile(id, newName.trim())
-        for (const session of activeSessions) {
-            await this.sessionFactory.getSessionService(session.type).start(session.sessionId)
-        }
+    const namedProfilesNames = this.getNamedProfiles().map((namedProfile) => namedProfile.name);
+    if (namedProfilesNames.includes(trimmedName)) {
+      return "Profile already exists";
     }
-
-    async deleteNamedProfile(id: string) {
-        const sessions = this.getSessionsWithNamedProfile(id)
-        const defaultNamedProfileId = this.repository.getDefaultProfileId()
-
-        for (const session of sessions) {
-            const sessionService = this.sessionFactory.getSessionService(session.type)
-            const wasActive = session.status === SessionStatus.active
-            if (wasActive) {
-                await sessionService.stop(session.sessionId)
-            }
-
-            (session as any).profileId = defaultNamedProfileId
-            this.repository.updateSession(session.sessionId, session)
-            this.workspaceService.updateSession(session.sessionId, session)
-
-            if (wasActive) {
-                await sessionService.start(session.sessionId)
-            }
-        }
-        this.repository.removeProfile(id)
-    }
-
-    getNewId() {
-        return uuid.v4()
-    }
-
-    validateNewProfileName(name: string): boolean | string {
-        const trimmedName = name.trim()
-        if (trimmedName.length === 0) {
-            return 'Empty profile name'
-        }
-        if (trimmedName === constants.defaultAwsProfileName) {
-            return '"default" is not a valid profile name'
-        }
-        const namedProfilesNames = this.getNamedProfiles().map(namedProfile => namedProfile.name)
-        if (namedProfilesNames.includes(trimmedName)) {
-            return 'Profile already exists'
-        }
-        return true
-    }
+    return true;
+  }
 }
