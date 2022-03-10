43e2153a6c2ef6d1de1f9ecf7546567f553fb58d
Eric Villa <eric.villa91@gmail.com>
2022-03-10 10:28:16 +0100

    test: refactored first RotationService test using Jest

--- core/services/rotation-service.spec.ts
+++ core/services/rotation-service.spec.ts
@@ -1,3 +1,40 @@
+import { describe, test, expect, jest } from "@jest/globals";
+import { Session } from "../models/session";
+import { SessionStatus } from "../models/session-status";
+import { RotationService } from "./rotation-service";
+import { SessionType } from "../models/session-type";
+jest.mock("../models/session");
+
+describe("RotationService", () => {
+  test("rotate - should rotate an active session when expired", () => {
+    const sessionActive: any = {};
+    sessionActive.status = SessionStatus.active;
+    sessionActive.type = SessionType.awsIamUser;
+    sessionActive.expired = jest.fn(() => true);
+
+    const expectedSessions: Session[] = [sessionActive];
+    const repository = {
+      listActive: () => expectedSessions,
+    } as any;
+
+    const awsIamUserService: any = {
+      name: "IamUser",
+      rotate: jest.fn(async () => {}),
+    };
+
+    const sessionFactory: any = {};
+    sessionFactory.getSessionService = jest.fn(() => awsIamUserService);
+
+    const rotationService = new RotationService(sessionFactory, repository);
+    rotationService.rotate();
+
+    expect(sessionActive.expired).toBeCalledTimes(1);
+    expect(sessionFactory.getSessionService).toBeCalledTimes(1);
+    expect(sessionFactory.getSessionService).toHaveBeenCalledWith(sessionActive.type);
+    expect(awsIamUserService.rotate).toBeCalledTimes(1);
+  });
+});
+
 /*
 describe('RotationService', () => {
   let rotationService: RotationService;
