4565c8fd9e7eb3a72b09b84d28951bab2f7dc9a4
Alessandro Gaggia <alex@besharp.it>
2022-03-22 15:10:18 +0100

    test: partial tests of repository

--- core/services/repository.spec.ts
+++ core/services/repository.spec.ts
@@ -3,12 +3,16 @@ import { Repository } from "./repository";
 import { FileService } from "./file-service";
 import { Workspace } from "../models/workspace";
 import { constants } from "../models/constants";
+import { Session } from "../models/session";
+import { SessionType } from "../models/session-type";
+import { SessionStatus } from "../models/session-status";
 
-describe("WebConsoleService", () => {
+describe("Repository", () => {
   let mockedWorkspace;
   let mockedNativeService;
   let mockedFileService;
   let repository;
+  let mockedSession: Session;
 
   beforeEach(() => {
     mockedWorkspace = new Workspace();
@@ -43,6 +47,15 @@ describe("WebConsoleService", () => {
 
     repository = new Repository(mockedNativeService, mockedFileService);
     (repository as any)._workspace = mockedWorkspace;
+
+    mockedSession = {
+      region: "eu-west-1",
+      sessionId: "123456789",
+      sessionName: "mock-session",
+      status: 0,
+      type: SessionType.awsIamUser,
+      expired: (): boolean => false,
+    };
   });
 
   test("get workspace() - returns the private workspace", () => {
@@ -84,5 +97,131 @@ describe("WebConsoleService", () => {
 
     expect(mockedFileService.writeFileSync).not.toBe(null);
     expect(mockedFileService.writeFileSync).toHaveBeenCalledWith("/" + constants.lockFileDestination, JSON.stringify(workspace));
+  });
+
+  test("getSessions() - get the sessions persisted in the workspace", () => {
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    expect(mockedFileService.writeFileSync).not.toBe(null);
+    expect(mockedFileService.writeFileSync).toHaveBeenCalledWith("/" + constants.lockFileDestination, JSON.stringify(workspace));
+    expect(repository.getSessions()).toStrictEqual([mockedSession]);
+  });
+
+  test("getSessionById() - get a session give an ID", () => {
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    expect(repository.getSessionById("123456789")).toStrictEqual(mockedSession);
+  });
+
+  test("addSession() - add a new session to the array of sessions", () => {
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    const mockedSession2: Session = {
+      region: "eu-west-2",
+      sessionId: "987654321",
+      sessionName: "mocked-2",
+      status: 0,
+      type: SessionType.awsIamUser,
+      expired: (): boolean => false,
+    };
+    repository.addSession(mockedSession2);
+
+    expect(repository.getSessions()).toStrictEqual([mockedSession, mockedSession2]);
+  });
+
+  test("updateSession() - updates a specific session", () => {
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    const mockedSession2: Session = {
+      region: "eu-west-2",
+      sessionId: "123456789",
+      sessionName: "mocked-2",
+      status: 0,
+      type: SessionType.awsIamUser,
+      expired: (): boolean => false,
+    };
+    repository.updateSession("123456789", mockedSession2);
+
+    expect(repository.getSessionById("123456789")).not.toBe(mockedSession);
+    expect(repository.getSessionById("123456789")).toBe(mockedSession2);
+    expect(repository.getSessions()).toStrictEqual([mockedSession2]);
+  });
+
+  test("updateSessions() - bulk updates all sessions at once", () => {
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    const mockedSession2: Session = {
+      region: "eu-west-2",
+      sessionId: "123456789",
+      sessionName: "mocked-2",
+      status: 0,
+      type: SessionType.awsIamUser,
+      expired: (): boolean => false,
+    };
+    repository.updateSessions([mockedSession2]);
+    expect(repository.getSessions()).not.toStrictEqual([mockedSession]);
+    expect(repository.getSessions()).toStrictEqual([mockedSession2]);
+  });
+
+  test("deleteSession() - remove a session from the array of sessions", () => {
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    repository.deleteSession("123456789");
+    expect(repository.getSessions()).not.toStrictEqual([mockedSession]);
+    expect(repository.getSessions()).toStrictEqual([]);
+  });
+
+  test("listPending() - list sessions in pending state", () => {
+    mockedSession.status = SessionStatus.pending;
+    const workspace = new Workspace();
+    workspace.sessions = [mockedSession];
+
+    mockedFileService.encryptText = jest.fn(() => JSON.stringify(workspace));
+
+    repository.workspace = workspace;
+    repository.persistWorkspace(workspace);
+
+    expect(repository.listPending()).toStrictEqual([mockedSession]);
+
+    mockedSession.status = SessionStatus.active;
+    workspace.sessions = [mockedSession];
+
+    expect(repository.listPending()).toStrictEqual([]);
   });
 });
