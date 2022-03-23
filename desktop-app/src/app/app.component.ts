51f521496487a0947d5465f9d5096ea477afd26f
Alessandro Gaggia <alex@besharp.it>
2022-03-23 16:10:51 +0100

    chore: removed calling for loader in app component

--- desktop-app/src/app/app.component.ts
+++ desktop-app/src/app/app.component.ts
@@ -147,7 +147,6 @@ export class AppComponent implements OnInit {
     // Go to initial page if no sessions are already created or
     // go to the list page if is your second visit
     await this.router.navigate(["/dashboard"]);
-    document.querySelector("#loader").classList.add("disable-loader");
 
     this.cliCommunicationService.startServer();
   }
