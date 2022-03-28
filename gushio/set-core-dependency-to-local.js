0d09c674dbefb111e4cf2cfc1bf1f009ff8eff0b
Eric Villa <eric.villa91@gmail.com>
2022-03-28 17:53:44 +0200

    chore: fixed package.json

--- gushio/set-core-dependency-to-local.js
+++ gushio/set-core-dependency-to-local.js
@@ -11,14 +11,14 @@ module.exports = {
     const cliCoreDependencyValue = cliPackageJson["dependencies"]["@noovolari/leapp-core"];
     if (cliCoreDependencyValue.indexOf("file:") === -1) {
       cliPackageJson["dependencies"]["@noovolari/leapp-core"] = "file:../core/dist";
-      fs.writeFileSync("cli/package.json", JSON.stringify(cliPackageJson, null, 2))
+      //fs.writeFileSync("cli/package.json", JSON.stringify(cliPackageJson, null, 2))
     }
 
     const desktopAppPackageJson = JSON.parse(fs.readFileSync("desktop-app/package.json"));
     const desktopAppCoreDependencyValue = desktopAppPackageJson["dependencies"]["@noovolari/leapp-core"];
     if (desktopAppCoreDependencyValue.indexOf("file:") === -1) {
       desktopAppPackageJson["dependencies"]["@noovolari/leapp-core"] = "file:../core/dist";
-      fs.writeFileSync("desktop-app/package.json", JSON.stringify(desktopAppPackageJson, null, 2))
+      //fs.writeFileSync("desktop-app/package.json", JSON.stringify(desktopAppPackageJson, null, 2))
     }
   }
 }
